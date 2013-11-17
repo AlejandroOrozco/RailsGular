6fc61baaacc97446f5d0bb7edc71dc3de692fb2c
Alejandro Orozco Builes <Alejandro@MacBook-Pro-de-Alejandro.local>
2013-11-17 11:28:06 -0500

    Intregración de angular a rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailGular::Application.config.secret_token = '7b9c877e34fd9b40ba360df506dc2d5c27b20e5da1e928cc3fe5f3ac4146f2eb193d1fe4138b8f4a540cbce73054c2f5a724833443d68faab24bf95f4eb0a8c8'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+RailGular::Application.routes.draw do
+	root :to => 'home#index'
+end
